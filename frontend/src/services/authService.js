6678ed76875763a6d90a028c3ea058f467aca82f
Jhuvizier Yamaguchi <tech18@sumomedia.co>
2024-09-05 23:38:54 +0800

    temporary push for login and register

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -3,18 +3,18 @@ import { api } from 'src/boot/axios'
 const authService = {
   async login(form) {
     await api.get('/sanctum/csrf-cookie');
-    const response = await api.post('/api/login', form);
+    const response = await api.post('/api/auth/login', form);
     return response.data;
   },
 
   async register(form) {
     await api.get('/sanctum/csrf-cookie');
-    const response = await api.post('/api/register', form);
+    const response = await api.post('/api/auth/register', form);
     return response.data;
   },
 
-  async getUser() {
-    const response = await api.get('/api/user');
+  async logout() {
+    const response = await api.post('/api/auth/logout');
     return response.data;
   },
 };
